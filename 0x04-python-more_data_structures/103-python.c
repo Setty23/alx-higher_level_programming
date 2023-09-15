f21228671ccdca2d801eee49d92bb9ed3be0665d
setty23 <settytek@gmail.com>
2023-09-15 11:51:46 -0700

    update

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -3,10 +3,12 @@
 
 /**
  * print_python_bytes - Prints bytes information
+ * @p: Python Object.
  *
- * @p: Python Object
+ *
  * Return: no return
  */
+
 void print_python_bytes(PyObject *p)
 {
 	char *string;
@@ -43,10 +45,12 @@ void print_python_bytes(PyObject *p)
 
 /**
  * print_python_list - Prints list information
+ * @p: PythonObject
  *
- * @p: Python Object
+ *
  * Return: no return
  */
+
 void print_python_list(PyObject *p)
 {
 	long int size, i;
